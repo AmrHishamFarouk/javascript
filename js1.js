4ce968bb07eea5cdad1f68b84de6d4dd92e0672b
AmrHishamFarouk <hishamamr121@gmail.com>
2023-10-27 23:26:52 +0200

    Add files via upload

--- js1.js
+++ js1.js
@@ -1,6 +1,18 @@
-let max = 57
-,   actual= max -13
-,   percentage= actual/max
-console.log(percentage)
-var amr = 22;
-let status = 'github 4e7ata'
+const name = 'amr'
+//doesnot embed
+const try1 = 'i am ${name}'
+console.log(try1)
+//also doesnot embed
+const try2 = "i am ${name}"
+console.log(try2)
+
+const name1 = "amr";
+const try3 = `i am ${name1}`;
+console.log(try3);
+
+const newline = `amr is me 
+and donot ask more`
+console.log(newline)
+
+const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);
